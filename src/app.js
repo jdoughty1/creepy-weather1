aa3ce525aa4a25b36ed06d99ef3163bdcd778faf
jdoughty1 <sassy128@gmail.com>
2022-01-19 11:24:10 -0700

    update js and html

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+
+ function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -23,12 +24,67 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
 
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 
-let apiKey = "efb9685e5e87bbfea508cb52e9192f88";
-let apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=${apikey}&units=metric';
-axios.get(apiUrl).then(displayTemperture)
+  document.querySelector("#precipitation").innerHtml = Math.round(
+    response.data.main.precipitation
+  );
+}
 
+function searchCity(city) {
+  let apiKey = "efb9685e5e87bbfea508cb52e9192f88";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
 
- //apikey=efb9685e5e87bbfea508cb52e9192f88
- //axios link=<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "efb9685e5e87bbfea508cb52e9192f88";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Denver");
