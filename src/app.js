faab68041e2cebd1f6a55f4d8b7fb80789b497ff
jdoughty1 <sassy128@gmail.com>
2022-02-05 12:54:20 -0700

    fixing

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ function formatDate(timestamp) {
 }
 
 function formatDay(timestamp) {
-  let date =newDate(timestaml * 1000);
+  let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
