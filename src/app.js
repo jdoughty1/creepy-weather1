10e2c5dec84d9d755c61b87a86189dcc67299e53
jdoughty1 <sassy128@gmail.com>
2022-02-12 14:01:11 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -108,16 +108,16 @@ function displayFahrenheitTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
 
   // remove the active class from the celsius link and add to fehrenheit link
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let celsiusTemperature = (celsiusTemperature (-32) / 1.8);
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
